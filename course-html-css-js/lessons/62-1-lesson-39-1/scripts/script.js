ef3c37cc9d8f0cb56f04df8551106c9768f30279
Artem Kutsan <artemkutsan@gmail.com>
2025-12-15 09:53:59 +0100

    content: Добавлен контент 62.1 Урок 39.1

---
+++ course-html-css-js/lessons/62-1-lesson-39-1/scripts/script.js
@@ -0,0 +1,66 @@
+/* course-html-css-js/lessons/62-1-lesson-39-1/scripts/script.js */
+
+import { fetchAsText } from '../../../../scripts/fetch-as-text.js';
+import { highlightPreBlocks } from '../../../../scripts/shiki-pre.js';
+
+/* ===== START ===== */
+// 62.1 Lesson 39.1
+/*  */
+const openModalBtn = document.querySelector('#open-modal');
+const closeModalBtn = document.querySelector('#close-modal');
+const modal = document.querySelector('.modal');
+const modalBody = document.querySelector('.modal-body');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+};
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+modalBody.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+/* ===== END ===== */
+
+const resourceUrl1 = `./scripts/script.js`;
+const resourceUrl2 = `./styles/styles.css`;
+
+fetchAsText(resourceUrl1, resourceUrl2).then(async (results) => {
+  const codeBlock = document.querySelector('#code-block');
+  if (!codeBlock) return;
+
+  for (let result of results) {
+    let content = result.text; // текст из объекта
+    const url = result.url; // URL для дальнейшего использования в extension
+
+    const pre = document.createElement('pre');
+
+    // Берем только контент между START и END
+    const match = content.match(
+      /(\/\*\s*===== START =====\s*\*\/|<!--\s*===== START =====\s*-->)([\s\S]*?)(\/\*\s*===== END =====\s*\*\/|<!--\s*===== END =====\s*-->)/
+    );
+
+    if (match) content = match[2].trim();
+
+    // Удаляем из комментариев вида /* */ пеереносы строк (можно отключить)
+    content = content.replace(/\/\*[\s\S]*?\*\//g, (block) => block.replace(/\r?\n+/g, ''));
+
+    pre.textContent = content;
+    const extension = url.split('.').pop();
+    pre.classList.add(`language-${extension}`);
+    codeBlock.appendChild(pre);
+  }
+  await highlightPreBlocks(codeBlock);
+});
