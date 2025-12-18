ab8aaf9d993367b3e3acdd063befced6a03b718c
Artem Kutsan <artemkutsan@gmail.com>
2025-12-18 09:50:32 +0100

    content: Добавлен контент 64.1 Урок 40.1

---
+++ course-html-css-js/lessons/64-1-lesson-41-1/scripts/script.js
@@ -0,0 +1,51 @@
+/* course-html-css-js/lessons/64-1-lesson-41-1/scripts/script.js */
+
+import { fetchAsText } from '../../../../scripts/fetch-as-text.js';
+import { highlightPreBlocks } from '../../../../scripts/shiki-pre.js';
+
+/* ===== START ===== */
+// 64.1 Lesson 41.1
+/*  */
+const sidebar = document.querySelector('.sidebar');
+const sidebarOpenBtn = document.querySelector('#sidebar-open');
+const sidebarCloseBtn = sidebar.querySelector('#sidebar-close');
+
+sidebarOpenBtn.addEventListener('click', () => {
+  sidebar.classList.remove('hidden');
+});
+
+sidebarCloseBtn.addEventListener('click', () => {
+  sidebar.classList.add('hidden');
+});
+/* ===== END ===== */
+
+const resourceUrl1 = `./scripts/script.js`;
+const resourceUrl2 = `./styles/styles.css`;
+
+fetchAsText(resourceUrl1, resourceUrl2).then(async (results) => {
+  const codeBlock = document.querySelector('#code-block');
+  if (!codeBlock) return;
+
+  for (let result of results) {
+    let content = result.text; // текст из объекта
+    const url = result.url; // URL для дальнейшего использования в extension
+
+    const pre = document.createElement('pre');
+
+    // Берем только контент между START и END
+    const match = content.match(
+      /(\/\*\s*===== START =====\s*\*\/|<!--\s*===== START =====\s*-->)([\s\S]*?)(\/\*\s*===== END =====\s*\*\/|<!--\s*===== END =====\s*-->)/
+    );
+
+    if (match) content = match[2].trim();
+
+    // Удаляем из комментариев вида /* */ пеереносы строк (можно отключить)
+    content = content.replace(/\/\*[\s\S]*?\*\//g, (block) => block.replace(/\r?\n+/g, ''));
+
+    pre.textContent = content;
+    const extension = url.split('.').pop();
+    pre.classList.add(`language-${extension}`);
+    codeBlock.appendChild(pre);
+  }
+  await highlightPreBlocks(codeBlock);
+});
